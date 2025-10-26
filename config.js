79efceac5e1611ac2866951e5aba74f089035101
iflaj <iflaj0913@gmail.com>
2025-10-26 19:37:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/iflaj/Support-Hub/blob/main/img/Host%20By%20Lk_20251006_222348_0000.png?raw=true",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 Support-Hub Is Alive Now😍*",
+BOT_OWNER: '94722136082',  // Replace with the owner's phone number
 
 
 
